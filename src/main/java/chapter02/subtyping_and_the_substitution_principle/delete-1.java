00b035b68fb2aa7a45cdd04ba04e2ee35662163d
mpn <maurice.naftalin@groupcdg.com>
2025-01-08 23:16:28 +0000

    delete class files

--- src/main/java/chapter02/subtyping_and_the_substitution_principle/delete-1.java
+++
@@ -1,7 +0,0 @@
-package chapter02.subtyping_and_the_substitution_principle;
-// ch02_1_1
-
-interface Collection<E> {
-  boolean add(E elt);
-
-}
--- src/main/java/chapter02/wildcards_with_extends/delete1.java
+++
@@ -1,7 +0,0 @@
-package chapter02.wildcards_with_extends;
-// ch02_2_1
-
-interface Collection<E> {
-  public boolean addAll(Collection<? extends E> c);
-
-}
