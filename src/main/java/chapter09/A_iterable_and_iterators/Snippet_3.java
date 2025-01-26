d088a14764ade89418d9e5df31d6d386f3fdbf34
mpn <maurice.naftalin@groupcdg.com>
2025-01-26 12:57:49 +0000

    code for chapters 9

---
+++ src/main/java/chapter09/A_iterable_and_iterators/Snippet_3.java
@@ -0,0 +1,15 @@
+package chapter09.A_iterable_and_iterators;
+// 11_1_4
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class Snippet_3 {
+	public static void main(String[] args)  {
+		List<String> strings = new ArrayList<>(List.of("alpha", "bravo", "charlie"));
+		List<String> modifiedStrings = strings.stream()
+		    .filter(s -> s.contains("r"))
+		    .collect(Collectors.toList());
+		assert modifiedStrings.equals(new ArrayList<>(List.of("bravo", "charlie")));
+	}
+}
